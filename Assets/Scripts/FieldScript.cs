bf0adfa1a1319de730a0abda8d57e3b8b9dcd4e3
Luke Spierewka <spierek@gmail.com>
2015-08-25 19:50:00 +0200

    Initial commit, grid generation

---
+++ Assets/Scripts/FieldScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FieldScript : MonoBehaviour {
+    #region Variables
+    public int x;
+    public int y;
+    #endregion
+
+    #region Monobehaviour
+    private void Awake () {
+    
+    }
+    #endregion
+
+    #region Methods
+    public void Set(int x, int y) {
+        gameObject.name = "Cell " + x + "," + y;
+        this.x = x;
+        this.y = y;
+    }
+    #endregion
+}
---
+++ Assets/Scripts/GridGenerator.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+public class GridGenerator : MonoBehaviour {
+    #region Variables
+    public static int       GridSize = 8;
+
+    [SerializeField]
+    private GameObject      fieldPrefab;
+    public float            gridSpacing = 5;
+
+    private FieldScript[,]  grid = new FieldScript[GridSize, GridSize];
+    #endregion
+
+    #region Monobehaviour
+    private void Awake () {
+        Generate();
+    }
+    
+    private void Update () {
+    
+    }
+    #endregion
+
+    #region Methods
+    public FieldScript GetGridCell(int x, int y) {
+        return grid[x, y];
+    }
+
+    private void Generate() {
+        float offset = ((GridSize - 1f) / (GridSize * 2f));
+
+        // generate grid
+        for (int i = 0; i < GridSize; ++i) {
+            for (int j = 0; j < GridSize; ++j) {
+                // spawn new cell
+                GameObject go = Instantiate(fieldPrefab) as GameObject;
+                go.transform.parent = transform;
+
+                grid[i, j] = go.GetComponent<FieldScript>();
+                grid[i, j].Set(i, j);
+
+                // spread out cells
+                Vector2 pos = new Vector2(
+                    (((float)i / GridSize) - offset) * gridSpacing,
+                    (((float)j / GridSize) - offset) * gridSpacing);
+                Debug.Log(pos);
+                go.transform.localPosition = pos;
+            }            
+        }
+    }
+    #endregion
+}
